2bf179e94e7b6967a3fac36022827b80fe6adedb
Elena Sokolovskaya <ev.sokolovskaya@gmail.com>
2023-10-09 12:56:46 -0700

    Added items to Array

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import React from 'react';
 const todoList = [
 {id:1,title: "Coding Assignment" },
 {id:2,title: "Mindset Assignment" },
-{id:3,title: "Mentor Assignment" },
+{id:3,title: "Mentor Session" },
+{id:4,title: "Mentor Assignment" },
+{id:5,title: "Group Session" },
 ];
 
 function App() {
