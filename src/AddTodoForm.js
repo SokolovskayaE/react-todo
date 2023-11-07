76043f8f5100b530902833bfa546c4f589ce5ee8
Elena Sokolovskaya <ev.sokolovskaya@gmail.com>
2023-11-06 18:06:54 -0800

    Move Todo List into State + Control Add Todo Input + Add New Todo to List + Destructure Props

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,17 +1,27 @@
-import React from "react"; // Import React from "react" npm package
+import React, { useState } from "react"; // Import React from "react" npm package
 
-function AddTodoForm(props){ //Add props as a parameter in the AddTodoForm function
+function AddTodoForm( { onAddTodo }) { 
+    const [todoTitle, setTodoTitle] = useState(""); // Create a new state variable named todoTitle with a setter setTodoTitle
+
+    function handleTitleChange(event) { // Declare a new function named handleTitleChange that takes event as a parameter 
+        const newTodoTitle = event.target.value; // Retrieve the input value from the event object
+        setTodoTitle(newTodoTitle); // Call the state setter setTodoTitle and pass newTodoTitle
+    }
+
     function handleAddTodo(event) { //Create a new function called handleAddTodo that takes event as a parameter 
         event.preventDefault(); //Prevent the default behavior of the form submit 
-        const todoTitle = event.target.title.value; //Retrieve the value of the title element from the event target and store it in a variable named todoTitle
         console.log("value is", todoTitle); //Log the value of todoTitle in the console
-        props.onAddTodo(todoTitle); //Invoke the onAddTodo callback prop and pass todoTitle as an argument
-        event.target.reset(); //Reset the form so the text input value is cleared
+        const todoItem = {
+            title: todoTitle,
+            id: Date.now()
+        };
+        onAddTodo(todoItem); // Update the onAddTodo callback prop to pass an Object instead of a String
+        setTodoTitle(""); // Remove the reset() method and replace it with logic to reset the todoTitle state to an empty String
     }
     return ( //Add a multi-line return statement to your AddTodoForm function//
     <form onSubmit={handleAddTodo}> {/*Create a <form> element + Add onSubmit prop to <form> element and pass the handleAddTodo function by reference*/}
         <label htmlFor="todoTitle">Title</label> {/*Add htmlFor attribute to <label> element with id "todoTitle"*/}
-        <input id="todoTitle" type="text" name="title"/> {/*Create a text <input> element with id "todoTitle" + Add a name attribute to the text input with value title*/}
+        <input id="todoTitle" type="text" name="title" value={todoTitle} onChange={handleTitleChange}/> {/*Create a text <input> element with id "todoTitle" + Add a name attribute to the text input with value title + Add value prop equal to todoTitle from component props*/}
         <button type="submit">Add</button> {/*Create a submit <button> element with text "Add"*/}
     </form>
     );
